5627198492bbf314777bfa6ad73705013dcf1d0a
Saroui <51461462+Saroui@users.noreply.github.com>
2020-11-24 22:51:27 -0500

    Create red.c

---
+++ redNeural/red.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "varcore.h"
+
+#define N 2
+#define M 4
+
+int main(){
+    float X[N][M] = {{0,0,1,1},{0,1,0,1}};
+    float Y[M] = {0,1,1,0};
+    
+    return 0;
+}
+
+float multiMat(float *X, float *B, int n, int m){
+    return 0;
+}
