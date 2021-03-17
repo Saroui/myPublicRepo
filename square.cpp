7045d7c77e11e3ddf0b62b25b94c08c5b75666be
arctic0630 <63928106+arctic0630@users.noreply.github.com>
2021-03-17 10:50:11 -0500

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -6,9 +6,10 @@
 int main(int argc, char **argv){
     double velocity
     
-    ros::init(argc, argv, "adelante");
+    ros::init(argc, argv, "Square");
     ros::NodeHandem nh;
     ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+    ros::Publisher pub2 = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_theta", 1000);
     ros::ServiceClient teleportClient = nh.serviceClient<turtlesim::TeleportAbsolute>("turtle1/teleport_absolute");
     
     turtlesim::TeleportAbsolute::Request req;
@@ -18,6 +19,8 @@ int main(int argc, char **argv){
     req.x = 0;
     req.y = 0;
     req.theta = 0;
+    double distancia = 0,
+    double dist_count = 0;
   
     
     const std::string PARAM_Velocity = "~velocity";
@@ -29,3 +32,27 @@ int main(int argc, char **argv){
         exit(1);
     }
 }
+
+    double t0 = ros::Time::now().toSec();
+   
+ while(ok){
+     
+    double t1 = ros::Time::now().toSec();
+     
+    distancia = velocity * (t1-t0);
+     
+    dist_counter += distancia;
+     
+    if(dist_counter >= 3){
+        
+        req.theta += 1.57;  
+        pub2.publish(req.theta);
+        pub.publish(velocity);
+        
+    }
+     
+ 
+ 
+ 
+ }
+
